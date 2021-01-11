70f8e56b35d1eb7255fa466a200f815d07a92a05
nikita-reva <68555738+nikita-reva@users.noreply.github.com>
2021-01-11 12:38:06 +0100

    New features added

--- public/app.js
+++ public/app.js
@@ -7,18 +7,28 @@ const userVideo = document.getElementById('user-video')
 const peerVideo = document.getElementById('peer-video')
 const roomInput = document.getElementById('room-name')
 const roomDisplay = document.getElementById('room-display')
+const roomControls = document.getElementById('room-controls')
+const muteButton = document.getElementById('mute')
+const hideButton = document.getElementById('hide')
+const leaveButton = document.getElementById('leave')
+
 let roomName
 let creator = false
 let rtcPeerConnection
 let userStream
+let muteflag = false
+let hideflag = false
 
 // Contains the stun server URL we will be using.
+
 const iceServers = {
 	iceServers: [
 		{ urls: 'stun:stun.services.mozilla.com' },
 		{ urls: 'stun:stun2.l.google.com:19302' },
 	],
 }
+
+// Buttons event listeners
 
 joinButton.addEventListener('click', function () {
 	roomName = roomInput.value
@@ -30,16 +40,76 @@ joinButton.addEventListener('click', function () {
 	}
 })
 
+muteButton.addEventListener('click', function () {
+	muteflag = !muteflag
+	if (muteflag) {
+		muteButton.textContent = 'Unmute'
+		muteButton.classList.add('active')
+		userStream.getTracks()[0].enabled = false
+	} else {
+		muteButton.textContent = 'Mute'
+		muteButton.classList.remove('active')
+		userStream.getTracks()[0].enabled = true
+	}
+})
+
+hideButton.addEventListener('click', function () {
+	hideflag = !hideflag
+	if (hideflag) {
+		hideButton.classList.add('active')
+		hideButton.textContent = 'Show'
+		userStream.getTracks()[1].enabled = false
+	} else {
+		hideButton.classList.remove('active')
+		hideButton.textContent = 'Hide'
+		userStream.getTracks()[1].enabled = true
+	}
+})
+
+leaveButton.addEventListener('click', function () {
+	if (userVideo.srcObject) {
+		userVideo.srcObject.getTracks().forEach((track) => track.stop())
+	}
+
+	if (peerVideo.srcObject) {
+		peerVideo.srcObject.getTracks().forEach((track) => track.stop())
+	}
+
+	userVideo.style.display = 'none'
+	peerVideo.style.display = 'none'
+
+	if (rtcPeerConnection) {
+		rtcPeerConnection.ontrack = null
+		rtcPeerConnection.onicecandidate = null
+		rtcPeerConnection.close()
+		rtcPeerConnection = null
+	}
+
+	userVideo.classList.remove('connected')
+	roomControls.classList.remove('active')
+	videoChatLobby.style.display = 'block'
+	roomDisplay.innerText = ''
+
+	socket.emit('leave', roomName)
+})
+
 // Triggered when a room is succesfully created.
+
 socket.on('created', function () {
+	console.log('Room Created')
 	creator = true
+	userVideo.style.display = 'block'
+	roomControls.classList.add('active')
 	getUserMedia()
 })
 
 // Triggered when a room is succesfully joined.
 
 socket.on('joined', function () {
+	console.log('Room Joined')
 	creator = false
+	userVideo.style.display = 'block'
+	roomControls.classList.add('active')
 	getUserMedia()
 })
 
@@ -58,15 +128,15 @@ socket.on('ready', function () {
 		rtcPeerConnection.ontrack = onTrackFunction
 		rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream)
 		rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream)
-		rtcPeerConnection.createOffer(
-			function (offer) {
+		rtcPeerConnection
+			.createOffer()
+			.then((offer) => {
 				rtcPeerConnection.setLocalDescription(offer)
 				socket.emit('offer', offer, roomName)
-			},
-			function (error) {
+			})
+			.catch((error) => {
 				console.log(error)
-			}
-		)
+			})
 	}
 })
 
@@ -87,15 +157,15 @@ socket.on('offer', function (offer) {
 		rtcPeerConnection.addTrack(userStream.getTracks()[0], userStream)
 		rtcPeerConnection.addTrack(userStream.getTracks()[1], userStream)
 		rtcPeerConnection.setRemoteDescription(offer)
-		rtcPeerConnection.createAnswer(
-			function (answer) {
+		rtcPeerConnection
+			.createAnswer()
+			.then((answer) => {
 				rtcPeerConnection.setLocalDescription(answer)
 				socket.emit('answer', answer, roomName)
-			},
-			function (error) {
+			})
+			.catch((error) => {
 				console.log(error)
-			}
-		)
+			})
 	}
 })
 
@@ -105,11 +175,34 @@ socket.on('answer', function (answer) {
 	rtcPeerConnection.setRemoteDescription(answer)
 })
 
+// Trigger when a we person coummuncated with has left the room
+
+socket.on('leave', function () {
+	creator = true
+
+	if (rtcPeerConnection) {
+		rtcPeerConnection.ontrack = null
+		rtcPeerConnection.onicecandidate = null
+		rtcPeerConnection.close()
+		rtcPeerConnection = null
+	}
+
+	if (peerVideo.srcObject) {
+		peerVideo.srcObject.getTracks().forEach((track) => track.stop())
+	}
+
+	peerVideo.style.display = 'none'
+	userVideo.classList.remove('connected')
+})
+
 ///////////////////////////////////////////////////
 // Functions
 
 function getUserMedia() {
-	const constraints = { audio: true, video: { width: 500, height: 500 } }
+	const constraints = {
+		audio: true,
+		video: { width: 500, height: 500, facingMode: 'user' },
+	}
 	navigator.mediaDevices
 		.getUserMedia(constraints)
 		.then(function (mediaStream) {
